dc975d4136d76ddde70ecdbd395e2d4175dfbe79
Miraclenetwork <thafiyaparveen47@gmail.com>
2026-01-12 13:20:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ SHORT_TUT = "https://t.me/How_to_Download_7x/26"
 
 # Bot Configuration
 SESSION = "yato"
-TOKEN = "642712"
-API_ID = ""
-API_HASH = ""
+TOKEN = "8381837271:AAHdZ8TC6cVtXoAkxtXWRa0yq0o_ChtpEzM"
+API_ID = "27806628"
+API_HASH = "25d88301e886b82826a525b7cf52e090"
 WORKERS = 5
 
-DB_URI = "mongodb"
+DB_URI = "mongodb+srv://Bossaahubdb:pmDPc44XLRB097vm@cluster0.unuj1k8.mongodb.net/?appName=Cluster0"
 DB_NAME = "yato"
 
 FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
